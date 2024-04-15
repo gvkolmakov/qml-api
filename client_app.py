b64f6af3eade8d6430bf5b69190fd2b6f0c00fac
German <gvkolmakov@gmail.com>
2024-04-15 09:22:14 -0400

    Update client_app.py
    Adding https endpoint.

--- client_app.py
+++ client_app.py
@@ -4,7 +4,7 @@ import ngq
 
 # Upload files to the server:
 body_upload = {
-    'endpoint':     'http://24.199.84.84:5000/upload_data',  # DO server
+    'endpoint':     'https://my-qml.org:8443/upload_data',  # DO server
     'api_key':      'Test725',                    # get it from the registration
     'dataset_dir':  'my_datasets/MNIST_1024_imgs/',  # from where to upload
     'data_id':      'MNIST_1024_imgs'                # name your dataset for the reference
@@ -15,7 +15,7 @@ response = ngq.upload_dataset_to_server_api( body_upload )
 
 # Train model on data on the server:
 body_train = {
-    'endpoint':     'http://24.199.84.84:5000/train_model_on_data',  # DO server
+    'endpoint':     'https://my-qml.org:8443/train_model_on_data',  # DO server
     'api_key':      'Test725',        # get it from the registration
     'data_id':      'MNIST_1024_imgs',   # name of the training dataset
     'model_name':   'my_first_model',   # name your model for the future reference
@@ -26,7 +26,7 @@ response = ngq.train_model_on_data_api( body_train )
 
 
 body_get_labels = {
-    'endpoint':     'http://24.199.84.84:5000/get_labels',  # DO server
+    'endpoint':     'https://my-qml.org:8443/get_labels',  # DO server
     'api_key':      'Test725',                    # get it from the registration
     'dataset_dir':  'my_datasets/just_one_img/',
     'model_name':   'my_first_model',               # your trained model
