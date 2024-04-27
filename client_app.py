327e51e6c81e9e8bdaf9b9f9599934f08db531be
German <gvkolmakov@gmail.com>
2024-04-27 13:00:04 -0400

    Update client_app.py

--- client_app.py
+++ client_app.py
@@ -1,52 +1,59 @@
-import sys; sys.path.append('./')
+import sys, time; sys.path.append('./')
 import ngq
 
+#######################################################
+# Also needs "requests" as a dependency.              #
+# One-liner to install from a terminal is:            #
+#      python3 -m pip install requests                #
+# See for details: https://pypi.org/project/requests/ #
+#######################################################
 
-# Upload files to the server:
+# This app is configured for a test user 'Test725'
+# If you need persionalized settings or have any question, contact us at
+# email: german@ngq.io
+
+# 1. Upload files to the server:
 body_upload = {
-    'endpoint':     'https://my-qml.org:8443/upload_data',  # DO server
-    'user_id':      'Test725',                    # get it from the registration
-    'dataset_dir':  'my_datasets/MNIST_1024_imgs/',  # from where to upload
-    'data_id':      'MNIST_1024_imgs'                # name your dataset for the reference
+    'dataset_dir':  'my_datasets/MNIST_1024_imgs/',   # path to your dataset
+    'data_id':      'MNIST_1024_imgs',                # give your dataset a name
+    'endpoint':     'https://my-qml.org/upload_data', # server
+    'user_id':      'Test725'                         # test user id
 }
 response = ngq.upload_dataset_to_server_api( body_upload )
+print("\n==> File uploading response:\n", response)
 
 
-
-# Train model on data on the server:
+# 2. Train model on data on the server:
 body_train = {
-    'endpoint':     'https://my-qml.org:8443/train_model_on_data',  # DO server
-    'user_id':      'Test725',        # get it from the registration
-    'data_id':      'MNIST_1024_imgs',   # name of the training dataset
-    'model_name':   'my_first_model',   # name your model for the future reference
-    'num_classes':  '10'                # how many classes in data set
+    'data_id':      'MNIST_1024_imgs',  # name of your dataset
+    'model_name':   'my_first_model',   # give your model a name
+    'num_classes':  '10',               # how many classes in your data set
+    'endpoint':     'https://my-qml.org/train_model_on_data',  # server
+    'user_id':      'Test725'           # test user id
 }
 response = ngq.train_model_on_data_api( body_train )
+print("\n==> Training response:\n", response)
 
 
-
-body_get_labels = {
-    'endpoint':     'https://my-qml.org:8443/get_labels',  # DO server
-    'user_id':      'Test725',                    # get it from the registration
-    'dataset_dir':  'my_datasets/just_one_img/',
-    'model_name':   'my_first_model',               # your trained model
+# 3. Give the system 5 seconds to complete the model training, and then
+#    check if the training is done and the model is ready to use.
+time.sleep(5.0)
+body_check = {
+    'model_name':   'my_first_model',           # name of your model
+    'endpoint':     'https://my-qml.org/check_model_ready', # server
+    'user_id':      'Test725'                   # test user id
 }
-labels = ngq.get_labels_api( body_get_labels )
-
-print("\nHello, I received the labels from the server:")
-print(labels)
+response = ngq.check_model_ready_api( body_check )
+print("\n==> Check if model is already trained:\n", response)
 
 
-
-    
-    
-    
-    
-    
-    
-    
-
-    
-
-
-
+# 4. Get labels for the files you placed in the directory in 'dataset_dir'
+#    below. The path 'my_datasets/just_one_img/" is a github example.
+body_get_labels = {
+    'dataset_dir':  'my_datasets/just_one_img/', # path to files to get labeled
+    'model_name':   'my_first_model',            # your trained model
+    'endpoint':     'http://my-qml.org/get_labels',  # server
+    'user_id':      'Test725'                   # test user id
+}
+response = ngq.get_labels_api( body_get_labels )
+print("\n==> Hello, I received the following from the server:\n", response)
